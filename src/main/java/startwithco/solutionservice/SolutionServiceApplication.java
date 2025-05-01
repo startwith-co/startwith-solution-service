62c9d3bfa3fb67d6618b753733edb794ce90279a
euics <pjh575157@gmail.com>
2025-05-01 14:16:22 +0900

    Initial Commit

---
+++ src/main/java/startwithco/solutionservice/SolutionServiceApplication.java
@@ -0,0 +1,19 @@
+package startwithco.solutionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SolutionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/startwithco/solutionservice/SolutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package startwithco.solutionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
